f19d4e75fdbb755bace9cbb3bbd70f35cef6dd7c
Tharun2331 <tharunsd23@gmail.com>
2025-08-27 01:13:16 -0400

    cron file modified

--- src/cron.ts
+++ src/cron.ts
@@ -1,7 +1,7 @@
-import cron from "cron";
+import {CronJob} from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
   if(!process.env.API_URL) return;
   https
     .get(process.env.API_URL, (res) => {
